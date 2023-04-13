4483ee90eb2b237b9ba3a1ef44df1ead0b9705ae
Jacopo Gennaro Esposito <37371071+jacopoesposito@users.noreply.github.com>
2023-04-13 11:43:41 +0200

    Implemented LCS
    Implemented LCS using dynamic programming

---
+++ LCS/src/lib/lcs.hpp
@@ -0,0 +1,112 @@
+using namespace std;
+
+#include <string>
+#include <iostream>
+
+#ifndef LCS_CLASS
+#define LCS_CLASS
+
+class LCS{
+    
+    public:
+        LCS(string x, string y);
+        void printMatrix();
+        void calLCS();
+        void printLCS(int i, int j);
+        int getLCSLength();
+        int getRows();
+        int getCols();
+        ~LCS();
+    private:
+        string x, y;
+        int rows, cols;
+        int **matrix_score;
+        string **matrix_path;
+};
+
+
+LCS::LCS(string x, string y){
+    this->x=x;
+    this->y=y;
+
+    this->rows=x.length();
+    this->cols=y.length();
+    cout<< rows << " " << cols << endl;
+    matrix_score = new int*[rows];
+    for(int i=0; i<rows; i++)
+        matrix_score[i] = new int[cols];
+
+    matrix_path = new string*[rows];
+    for(int i=0; i<rows; i++)
+        matrix_path[i] = new string[cols];
+}
+
+int LCS::getLCSLength(){
+    return matrix_score[rows-1][cols-1];
+}
+
+int LCS::getRows(){
+    return rows;
+}
+
+int LCS::getCols(){
+    return cols;
+}
+
+void LCS::calLCS(){
+    for(int i = 0; i < rows; i++){
+        matrix_score[i][0]=0;
+    }
+    for(int i = 0; i < cols; i++){
+        matrix_score[0][i]=0;
+    }
+
+    for(int i = 1; i < rows; i++){
+        for(int j = 1; j < cols; j++){
+            if(x[i-1] == y[j-1]){
+                matrix_score[i][j]=matrix_score[i-1][j-1]+1;
+                matrix_path[i][j]="\\";
+            }
+            else if(matrix_score[i-1][j]>=matrix_score[i][j-1]){
+                matrix_score[i][j]=matrix_score[i-1][j];
+                matrix_path[i][j]="|";
+            }else{
+                matrix_score[i][j]=matrix_score[i][j-1];
+                matrix_path[i][j]="<";
+            }
+        }
+    }    
+}
+
+void LCS::printMatrix(){
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            cout << matrix_score[i][j];
+        }
+        cout<<endl;
+    }
+
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            cout << matrix_path[i][j] << " ";
+        }
+        cout<<endl;
+    }
+}
+
+void LCS::printLCS(int i, int j){
+    if(i == 0 || j == 0)
+        return;
+    if(matrix_path[i][j]=="\\"){
+        printLCS(i-1, j-1);
+        cout << x[i-1];
+    }
+    else if(matrix_path[i][j]=="|"){
+        printLCS(i-1, j);
+    }
+    else{
+        printLCS(i, j-1);
+    }
+    
+}
+#endif 
---
+++ LCS/src/main.cpp
@@ -0,0 +1,22 @@
+using namespace std;
+
+#include <iostream>
+#include "./lib/lcs.hpp"
+
+int main(int argc, char* argv[]){
+
+    
+
+    if(argc < 3){
+        cout << "Errore input" << endl;
+        return -1;
+    }
+    string x = argv[1];
+    string y = argv[2];
+    LCS *Lcs = new LCS(x, y);
+    Lcs->calLCS();
+    Lcs->printMatrix();
+    cout << "Lunghezza LCS-> " << Lcs->getLCSLength() << "LCS String: " << endl;
+    Lcs->printLCS(Lcs->getRows()-1, Lcs->getCols()-1);
+
+}
